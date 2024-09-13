5067b8ea78a003e725713b3fbdc86c51a9377532
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-13 16:11:57 +0530

    Added: UC-1 Create a regex pattern to validate pin code

--- src/main/java/com/regex/problem/Regex.java
+++ src/main/java/com/regex/problem/Regex.java
@@ -1,9 +1,24 @@
 package com.regex.problem;
 
+import java.util.regex.Pattern;
+
 public class Regex
 {
+    public void validatePincode() {
+        String pincode="400088";
+        String regex="^[1-9][0-9]{5}$";
+        boolean answer = Pattern.matches(regex,pincode);
+        if (answer) {
+            System.out.println("Pincode is Valid");
+        }
+        else {
+            System.out.println("Pincode is Invalid");
+        }
+    }
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Regex Problem" );
+        Regex regex = new Regex();
+        regex.validatePincode();
     }
 }
