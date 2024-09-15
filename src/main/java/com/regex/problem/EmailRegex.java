56fcbd33bb4c23d553cbeb86d09cd28b40f9eb0e
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-15 21:56:37 +0530

    Added: UC-1 Validate email with regex email start with abc

---
+++ src/main/java/com/regex/problem/EmailRegex.java
@@ -0,0 +1,22 @@
+package com.regex.problem;
+
+import java.util.regex.Pattern;
+
+public class EmailRegex {
+    public void validateEmail(String email) {
+        String regex="^[a-z]{3}.*$";
+        boolean answer = Pattern.matches(regex,email);
+        if (answer) {
+            System.out.println("Email is Valid");
+        }
+        else {
+            System.out.println("Email is Invalid");
+        }
+    }
+    public static void main( String[] args )
+    {
+        EmailRegex regex = new EmailRegex();
+        String email = "abc.xyz@bridgelabz.co.in";
+        regex.validateEmail(email);
+    }
+}
