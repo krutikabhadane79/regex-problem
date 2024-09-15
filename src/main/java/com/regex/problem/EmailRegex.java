520188b5808385d6e4fc6eab78e675f846024e70
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-15 22:25:32 +0530

    Added: UC-5 Finally close the expression with supporting optional part

--- src/main/java/com/regex/problem/EmailRegex.java
+++ src/main/java/com/regex/problem/EmailRegex.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class EmailRegex {
     public void validateEmail(String email) {
-        String regex="^[a-z]{3}([_+\\-.][a-z]+)?@[a-z]{10}\\.[a-z]{2}.*$";
+        String regex="^[a-z]{3}([_+\\-.][a-z]+)?@[a-z]{10}\\.[a-z]{2}.(\\.[a-z]{2})?$";
         boolean answer = Pattern.matches(regex,email);
         if (answer) {
             System.out.println("Email is Valid");
@@ -16,7 +16,13 @@ public class EmailRegex {
     public static void main( String[] args )
     {
         EmailRegex regex = new EmailRegex();
-        String email = "abc-xyz@bridgelabz.co.in";
-        regex.validateEmail(email);
+        String email1 = "abc@bridgelabz.co.in";
+        String email2 = "abc.xyz@bridgelabz.co";
+        String email3 = "abc.xyz@bridgelabz.co.uk";
+        String email4 = "abc.xyz@bridgelabz";
+        regex.validateEmail(email1);
+        regex.validateEmail(email2);
+        regex.validateEmail(email3);
+        regex.validateEmail(email4);
     }
 }
